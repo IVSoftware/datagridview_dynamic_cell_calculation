48d45abc80d8c3cdb12092e2b54dc098b4f67480
Tom Gregor <tgregor@ivsoftware.com>
2022-09-13 08:41:09 -0600

    b4 simplify

--- datagridview_dynamic_cell_calculation/MainForm.cs
+++ datagridview_dynamic_cell_calculation/MainForm.cs
@@ -17,21 +17,26 @@ namespace datagridview_dynamic_cell_calculation
         public MainForm()
         {
             InitializeComponent();
-            dgv_Filho.CellEndEdit += dgv_Filho_CellEndEdit_1;
             cb_Iva.SelectedIndex = 0;
         }
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
             dgv_Filho.DataSource = DataSource;
+            DataSource.ListChanged += (sender, e) =>
+            {
+                if(e.ListChangedType == ListChangedType.ItemChanged)
+                {
+                    dgv_Filho.Refresh();
+                }
+            };
             // Add one or more items to autogenerate the columns.
             for (int i = 1; i <= 5; i++)
             {
                 DataSource.Add(new Articulo 
                 { 
-                    Descricao = $"Articulo {i}",
+                    Descricao = $"Articulo {(char)('A' + (i - 1))}",
                     Medida_1 = i,
-                    Medida_2 = (decimal)(i * 1.5),
                 });
             }
             foreach (DataGridViewColumn column in dgv_Filho.Columns)
@@ -45,6 +50,9 @@ namespace datagridview_dynamic_cell_calculation
                     case nameof(Articulo.Check_Filho):
                         column.Width = 50;
                         column.HeaderText = string.Empty;
+                        break;
+                    case nameof(Articulo.Medida_1):
+                        column.DefaultCellStyle.Format = "F2";
                         break;
                     default:
                         column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
@@ -66,8 +74,8 @@ namespace datagridview_dynamic_cell_calculation
             {
                 if (!string.IsNullOrWhiteSpace(tb_CodigoArtigo.Text) || !string.IsNullOrWhiteSpace(tb_Descricao.Text))
                 {
-                    dgv_Filho.CurrentRow.Cells["ArtigoPai"].Value = tb_CodigoArtigo.Text;
-                    dgv_Filho.CurrentRow.Cells["Descricao_Pai"].Value = tb_Descricao.Text + " " + Medida_1 + Medida_2;
+                    // dgv_Filho.CurrentRow.Cells["ArtigoPai"].Value = tb_CodigoArtigo.Text;
+                    // dgv_Filho.CurrentRow.Cells["Descricao_Pai"].Value = tb_Descricao.Text + " " + Medida_1 + Medida_2;
                     dgv_Filho.CurrentRow.Cells["CodigoArtigoFilho"].Value = tb_CodigoArtigo.Text + Medida_1 + Medida_2;
 
                     decimal PrecoFilho = Convert.ToDecimal(dgv_Filho.CurrentRow.Cells["PrecoFilhoSemIva"].Value);
@@ -84,9 +92,23 @@ namespace datagridview_dynamic_cell_calculation
                 }
             }
         }
+
     }
-    class Articulo
+    class Articulo : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            onAlterar();
+        }
+
+        private void onAlterar()
+        {
+            var sval = Medida_1.ToString("F2");
+            Descricao_Pai = $"{Descricao} {sval}";
+            CodigoArtigoFilho =  $"{CodigoArtigo} {sval}";
+        }
         public bool Check_Filho { get; set; }
         string _descricao = string.Empty;
         public string Descricao
@@ -101,33 +123,62 @@ namespace datagridview_dynamic_cell_calculation
                 }
             }
         }
-
-        public string ArtigoPai { get; set; }
-        public string Descricao_Pai { get; set; }
+        string _artigoPai = string.Empty;
+        public string ArtigoPai
+        {
+            get => _artigoPai;
+            set
+            {
+                if (!Equals(_artigoPai, value))
+                {
+                    _artigoPai = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string _descricao_Pai = string.Empty;
+        public string Descricao_Pai
+        {
+            get => _descricao_Pai;
+            private set
+            {
+                if (!Equals(_descricao_Pai, value))
+                {
+                    _descricao_Pai = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public decimal PrecoFilhoSemIva { get; set; }
         public decimal PrecoFilhoComIva { get; set; }
-        public decimal Medida_1 { get; set; }
-        public decimal Medida_2 { get; set; }
-        public string CodigoArtigo { get; } = System.Guid.NewGuid().ToString().Substring(0, 10).ToUpper();
-        public string CodigoArtigoFilho { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        decimal _medida1 = 0;
+        public decimal Medida_1
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            switch (propertyName)
+            get => _medida1;
+            set
             {
-                case nameof(Descricao):
-                    onDescricaoAlterar();
-                    break;
-                default:
-                    break;
+                if (!Equals(_medida1, value))
+                {
+                    _medida1 = value;
+                    OnPropertyChanged();
+                }
             }
         }
 
-        private void onDescricaoAlterar()
+        string _codigoArtigo = System.Guid.NewGuid().ToString().Substring(0, 10).ToUpper();
+        public string CodigoArtigo
         {
-            Descricao_Pai = $"{Descricao} {Medida_1 + Medida_2}";
+            get => _codigoArtigo;
+            set
+            {
+                if (!Equals(_codigoArtigo, value))
+                {
+                    _codigoArtigo = value;
+                    OnPropertyChanged();
+                }
+            }
         }
+        public string CodigoArtigoFilho { get; set; }
     }
 }
